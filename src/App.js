001e6811e29c1e89ffa87ba079dbf48d3949aba9
Igor Brizack <igor_brizack@hotmail.com>
2022-07-14 16:14:18 -0300

    roteamento e provider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Provider from './context/Provider';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        {/* <Route path="/foods" component={ Login } />
+        <Route path="/drinks" component={ Login } />
+        <Route path="/foods/:id" component={ Login } />
+        <Route path="/drinks/:id" component={ Login } />
+        <Route path="/foods/:id/in-progress" component={ Login } />
+        <Route path="/drinks/:id/in-progress" component={ Login } />
+        <Route path="/profile" component={ Login } />
+        <Route path="/done-recipes" component={ Login } />
+        <Route path="/favorite-recipes" component={ Login } /> */}
+      </Switch>
+    </Provider>
   );
 }
 
