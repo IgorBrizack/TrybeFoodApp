6de3c4b61edd137b94bd723af00bb7510c01c53a
Igor Brizack <igor_brizack@hotmail.com>
2022-07-15 14:49:52 -0300

    group 19 requisito 10 12

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import context from '../context/Context';
 import Header from '../components/Header';
 
 function Drinks() {
   const { history } = useHistory();
+  const { setIsFoodOrDrink } = useContext(context);
+
+  useEffect(() => {
+    setIsFoodOrDrink('Drinks');
+  }, []);
+
   return (
     <div>
       <Header page="Drinks" history={ history } />
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import context from '../context/Context';
 import Header from '../components/Header';
 
 function Foods() {
   const { history } = useHistory();
+  const { setIsFoodOrDrink } = useContext(context);
+
+  useEffect(() => {
+    setIsFoodOrDrink('Foods');
+  }, []);
+
   return (
     <div>
       <Header page="Foods" history={ history } />
