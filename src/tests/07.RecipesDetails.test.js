95d5d62ccb88d51de2f040e411de24aeb6059212
Igor Brizack <igor_brizack@hotmail.com>
2022-07-20 18:47:21 -0300

    testes recipes details

--- src/tests/07.RecipesDetails.test.js
+++ src/tests/07.RecipesDetails.test.js
@@ -9,6 +9,33 @@ import fetch from './helpers/mocks/fetch';
 describe('Faça implenetações de testes para validar o correto funcionamento da tela de Detalhes', () => {
   it('Verifique se ao clicar em um item da lista renderizada na tela, se há um redirecionamento para a tela de detalhes da receita', async () => {
     jest.spyOn(global, 'fetch').mockImplementation((url) => fetch(url) )
+    localStorage.removeItem('favoriteRecipes');
+    const localStorageMock = (() => {
+      let store = {};
+      return {
+      getItem(key) {
+      return store[key];
+      },
+      setItem(key, value) {
+      store[key] = value.toString();
+      },
+      clear() {
+      store = {};
+      },
+      removeItem(key) {
+      delete store[key];
+      }
+      };
+      })();
+      Object.defineProperty(window, 'localStorage', {
+      value: localStorageMock
+    });
+    localStorage.setItem('inProgressRecipes', JSON.stringify({
+      cocktails: {},
+      meals: {
+        52977: [false, false, false, false, false, false, false, false, false, false, false, false, false],
+      },
+    }));
 
     const history = createMemoryHistory();
     render(
@@ -17,14 +44,42 @@ describe('Faça implenetações de testes para validar o correto funcionamento d
       </Router>
     );
 
-    history.push("/foods")
-    console.log(history)
+    history.push("/foods/52977")
 
-    expect(await screen.findByRole('heading', {  name: /corba/i},{timeout: 5000})).toBeInTheDocument()
-    const corbaBTN =  screen.getByRole('heading', {  name: /corba/i})
-    userEvent.click(corbaBTN);
+    // expect(await screen.findByRole('heading', {  name: /corba/i},{timeout: 5000})).toBeInTheDocument()
+    // const corbaBTN =  screen.getByRole('heading', {  name: /corba/i})
+    // userEvent.click(corbaBTN);
 
     expect(await screen.findByTestId("instructions",{timeout: 5000})).toBeInTheDocument()
     expect(global.fetch).toHaveBeenCalled()
+
+    const favoriteBtn = screen.getByRole('img', {  name: /favoriteicon/i});
+    expect(favoriteBtn).toBeInTheDocument()
+    userEvent.click(favoriteBtn);
+    userEvent.click(favoriteBtn)
+
+    const shareButton = screen.getByRole('img', {  name: /shareicon/i});
+    expect(shareButton).toBeInTheDocument();
+    
+    // NÃO ENCONTRO O CONTINUE RECIPE
+    expect(await screen.findByRole('button', {  name: /continue recipe/i}, {timeout: 5000})).toBeInTheDocument()
+
+    history.push("/drinks")
+
+    expect(await screen.findByRole('heading', {  name: /gg/i},{timeout: 5000})).toBeInTheDocument()
+    const ggBTN =  screen.getByRole('heading', {  name: /gg/i})
+    userEvent.click(ggBTN);
+
+    expect(await screen.findByTestId("instructions",{timeout: 5000})).toBeInTheDocument()
+    expect(global.fetch).toHaveBeenCalled()
+
+    const favoriteBtnDrinks = screen.getByRole('img', {  name: /favoriteicon/i});
+    expect(favoriteBtnDrinks).toBeInTheDocument()
+    userEvent.click(favoriteBtnDrinks);
+    userEvent.click(favoriteBtnDrinks)
+
+    const shareButtonDrinks = screen.getByRole('img', {  name: /shareicon/i});
+    expect(shareButtonDrinks).toBeInTheDocument();
+    
   })
 })
