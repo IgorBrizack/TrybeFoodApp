36f2ee0edc4c457d8d00cfbded79f3aff914e614
lucaspaesj <lpjanuzi@gmail.com>
2022-07-20 18:16:52 -0300

    adicionando funções de adição ao storage e implementações anteriores

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import CardFood from '../components/CardFood';
+import CardDrink from '../components/CardDrink';
 
 function DoneRecipes() {
   const { history } = useHistory();
+  const storageItems = JSON.parse(localStorage.getItem('doneRecipes'));
   return (
     <div>
       <Header page="Done Recipes" history={ history } />
-      doneRecipes
+      <button type="button" data-testid="filter-by-all-btn">All</button>
+      <button type="button" data-testid="filter-by-food-btn">Food</button>
+      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      { storageItems
+      && (storageItems.map((item, index) => {
+        if (item.type === 'food') {
+          return (
+            <CardFood
+              meal={ item }
+              key={ item.id }
+              index={ index }
+              page="doneRecipes"
+            />
+          );
+        }
+        return (
+          <CardDrink
+            drink={ item }
+            key={ item.id }
+            index={ index }
+            page="doneRecipes"
+          />
+        );
+      })
+      ) }
     </div>
   );
 }
