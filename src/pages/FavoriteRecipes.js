a7b706db9ec0d92febfb8ead8a13649820071ae6
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2022-07-22 15:01:30 -0300

    requisito 50 concluido

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import CardFood from '../components/CardFood';
+import CardDrink from '../components/CardDrink';
+import FavoriteButton from '../components/FavoriteButton';
 
 function FavoriteRecipes() {
-  const history = useHistory();
+  const { history } = useHistory();
+  const storageItems = JSON.parse(localStorage.getItem('favoriteRecipes'));
   return (
     <div>
-      <Header page="Favorite Recipes" history={ history } />
-      <div>Favorite Recipes</div>
+      <Header page="Done Recipes" history={ history } />
+      <button type="button" data-testid="filter-by-all-btn">All</button>
+      <button type="button" data-testid="filter-by-food-btn">Food</button>
+      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      { storageItems
+      && (storageItems.map((item, index) => {
+        if (item.type === 'Foods') {
+          return (
+            <>
+              <CardFood
+                key={ item.id }
+                meal={ item }
+                page="favoriteRecipes"
+                index={ index }
+              />
+              <FavoriteButton dataItem={ item } type={ item.type } />
+            </>
+          );
+        }
+        return (
+          <>
+            <CardDrink
+              key={ item.id }
+              drink={ item }
+              page="favoriteRecipes"
+              index={ index }
+            />
+            <FavoriteButton dataItem={ item } type={ item.type } />
+          </>
+        );
+      })
+      ) }
     </div>
   );
 }
